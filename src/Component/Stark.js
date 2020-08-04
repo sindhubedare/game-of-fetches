1787dc40b32c8e41135cf62ed6ba104552d5800c
sindhubedare <sindhubedare1990@gmail.com>
2020-08-04 14:24:21 -0500

    stark working

--- src/Component/Stark.js
+++ src/Component/Stark.js
@@ -11,31 +11,28 @@ export default class Stark extends Component {
       data: []
     }
   }
-
   componentDidMount() {
-
-    Axios.get("https://www.anapioficeandfire.com/api/characters/209").then(res => {
-      const founderNameUrl = res.data.founder; 
-      return Axios.get(founderNameUrl)
-    })
+    Axios.get("https://www.anapioficeandfire.com/api/houses/362").then(res => {
+    const founderNameUrl=res.data.founder;
+    return Axios.get(founderNameUrl)
+     })
     .then(res => {
-        const founderName= res.data.name;
-        this.setState({data: founderName})
+    const founderName= res.data.name;
+    this.setState({data: founderName})
     })
     .catch(error => {
-      console.log('there is an error', error)
+    console.log('there is an error', error)
     })
-  
-  }
 
-  render() {
-    return (
-      <div>
-        <h2>7. What's the name of the founder of House Stark?</h2>
-        <h4>The founder of House Stark is {this.state.data}.</h4>        
-      </div>
-    )
-  }
+}
+render() {
+return (
+  <div>
+    <h2>7. What's the name of the founder of House Stark?</h2>
+    <h4>The name of the founder of House Stark is {this.state.data}.</h4>     
+  </div>
+)
+}
 }
 
 
